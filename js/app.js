e185d0a5c1a072b2b21d8e41c3c5e44a464db4ae
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-05 16:34:09 -0700

    functionality complete, style needed, more questions needed

--- js/app.js
+++ js/app.js
@@ -3,28 +3,29 @@ var score = 0;
 $('.question').mousedown( function() {
   //find the id of the element clicked on & convert to question;
   selectedQuestion = eval(this.id);
+  // var questionId = $(this).data('qid');
   //hide the question from the board
-  $(this).hide();
+  $(this).css('visibility', 'hidden');
   //hide the question board
   $('.board-wrap').hide();
   //show the individual question div
   $('.question-and-progress').show();
   //append the question to the div
-  $('#new-question').append("<p>" + selectedQuestion.question + "</p>");
+  $('#new-question').append("<p>" + selectedQuestion.question + "</p>").show();
   //iterate over the answers and append the possible answers as buttons
   for (i = 0; i < selectedQuestion.answers.length; ++i) {
-    $('#new-answers').append("<button id=" + i + ">" + selectedQuestion.answers[i] + "</button>");
+    $('#new-answers').append("<button id=" + i + ">" + selectedQuestion.answers[i] + "</button>").show();
   }
 
   $('button').mousedown( function() {
     var questionID = $(this).attr("id");
      if (questionID == selectedQuestion.correctAnswer) {
       score += selectedQuestion.points;
-      $("#player-feedback").append("<h3>" + selectedQuestion.correctReply + "</h3>").show();
+      $("#player-feedback").append("<h3>" + "Correct! " + selectedQuestion.reply + "</h3>").show();
     }
       else {
       score -= selectedQuestion.points;
-      $("#player-feedback").append("<h3>" + selectedQuestion.incorrectReply + "</h3>").show();
+      $("#player-feedback").append("<h3>" + "Incorrect. " + selectedQuestion.reply + "</h3>").show();
     }
     //remove question from the board
       $("#new-answers button").hide();
@@ -35,17 +36,16 @@ $('.question').mousedown( function() {
 
 });
 
-$("#back-to-board").mousedown( function() {
-    console.log("button Pressed");
+$("#player-feedback").on('click', '#back-to-board', function() {
     $(".board-wrap").show();
+    $("#player-feedback h3").remove();
+    $("#player-feedback p").remove();
     $(".question-and-progress").hide();
-  });
-
-$("body").on('click', '#back-to-board', function() {
-    console.log("button Pressed");
-    $(".board-wrap").show();
-    $(".question-and-progress").hide();
-  });
+    $("#new-question p").remove();
+    $("#new-answers").hide();
+    $("#new-answers button").remove();
+    $("#back-to-board").remove();
+});
 
   
 
--- js/questions.js
+++ js/questions.js
@@ -1,54 +1,100 @@
+var allQuestions = [question1, question2, question3];
+
+
 question1 = {
   image: "../assets/image.jpg",
   question: "Edward VII ascended to the throne upon the death of which monarch?",
   answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
   correctAnswer: 0,
   reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  // correctReply: "Correct!", 
-  // incorrectReply: "Incorrect.",
-  points: 100
+  points: 200
 };
 
 question2 = {
   //How do I store an image with a question? 
-  question: "Edward VII ascended to the throne upon the death of which monarch?",
-  answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
-  correctAnswer: 0,
-  // reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  correctReply: "Correct! Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  incorrectReply: "Incorrect. Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  points: 100
+  question: "The primary London home of Edward, as Prince of Wales, was:",
+  answers: ["Buckingham Palace", "Clarence House", "Marlborough House", "Kensington Palace"],
+  correctAnswer: 2,
+  reply: "Edward, as the eldest son of the sovereign, used Marlborough House as his London base until he was coronated King of Endland in 1901.",
+  points: 400
 };
 
 question3 = {
   //How do I store an image with a question? 
-  question: "Edward VII ascended to the throne upon the death of which monarch?",
-  answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
-  correctAnswer: 0,
-  reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  correctReply: "Correct!" + reply
-  incorrectReply: "Incorrect. Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  points: 100
+  question: "Edward's coronation was delayed because of a medical emergency.  It was:",
+  answers: ["Lung Cancer", "Gout", "Appendectomy", "Ruptured Spleen"],
+  correctAnswer: 2,
+  reply: "Two days before the June 26th scheduled coronation, he was diagnosed with appendicitis.  Edward was later crowned on 9 August 1902.",
+  points: 600
 };
 
 question4 = {
   //How do I store an image with a question? 
-  question: "Edward VII ascended to the throne upon the death of which monarch?",
-  answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
-  correctAnswer: 0,
-  // reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  correctReply: "Correct! Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  incorrectReply: "Incorrect. Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  points: 100
+  question: "At his funeral, Kind Edward VII's catafalque was followed by his dog, named:",
+  answers: ["Rover", "George", "Albert", "Caesar"],
+  correctAnswer: 3,
+  reply: "Caesar, a Wire Fox Terrier and long time companion of the King, walked in the funeral procession in prominence ahead of 9 kings and heads of state.",
+  points: 800
 };
 
 question5 = {
   //How do I store an image with a question? 
-  question: "Edward VII ascended to the throne upon the death of which monarch?",
-  answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
+  question: "Edward VII was succeeded by his eldest surviving son who reigned under what name?",
+  answers: ["Kind George V", "King Edward VIII", "King George VI", "King Albert I"],
   correctAnswer: 0,
-  // reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  correctReply: "Correct! Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  incorrectReply: "Incorrect. Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  points: 100
+  reply: "Though not his eldest son, that being Prince Albert Victor who had died in 1892, Edward was succeeded by his eldest surviving son, George, who ruled as George V.",
+  points: 1000
 };
+
+question6 = {
+  //How do I store an image with a question? 
+  question: "As a general rule, the highest ranking title in the British Peerage is:",
+  answers: ["Earl", "Baron", "Duke", "Marquesses"],
+  correctAnswer: 2,
+  reply: "The title of Duke out ranks all others.  Traditionally the ranking is: Duke, Marquesses, Earl, Viscount & Baron.",
+  points: 200
+};
+
+question7 = {
+  //How do I store an image with a question? 
+  question: "Titles, Estates, & Assets were always passed down to the closest living male member of the current holder of the title/estate.  Binding these together was known as:",
+  answers: ["Monarchy", "Soverignity", "Entail", "Testament"],
+  correctAnswer: 2,
+  reply: "The Entail ensures that a single male descendent gets all the family's real estate, title, and assets.",
+  points: 400
+};
+
+question8 = {
+  //How do I store an image with a question? 
+  question: "The male equivalent of 'Earl' is:",
+  answers: ["Princess", "Countess", "Duchess", "Baroness"],
+  correctAnswer: 1,
+  reply: "An Earl's wife is his Countess",
+  points: 600
+};
+
+question9 = {
+  //How do I store an image with a question? 
+  question: "In a marriage of titles, a wife outliving her husband will retain her title until her own death, but with what word appended to the beginning of it?",
+  answers: ["Matriarch", "Countess", "Dowager", "Widow"],
+  correctAnswer: 2,
+  reply: "The Countess, outliving her husband, would become the Dowager Countess, and the succeeding Earl's wife would become the Countess.",
+  points: 800
+};
+
+question10 = {
+  //How do I store an image with a question? 
+  question: "Until recently, not only titles, but inheritence flowed to the first born male child.  This is know as:",
+  answers: ["Seniority", "Ultimogeniture", "Male First", "Male Primogeniture"],
+  correctAnswer: 3,
+  reply: "Male Priomgeniture was the first-born male heir.  Today, generally, Priomgeniture is the allowed rule, meaning oldest heir, regardless of gender.",
+  points: 1000
+};
+
+
+
+
+
+
+
+
