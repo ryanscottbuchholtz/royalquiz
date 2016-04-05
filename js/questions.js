383ce8c6e761281577443b7c502fb0abce39fc84
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-05 12:29:14 -0700

    update index

--- js/questions.js
+++ js/questions.js
@@ -1,11 +1,11 @@
 question1 = {
-  //How do I store an image with a question? 
+  image: "../assets/image.jpg",
   question: "Edward VII ascended to the throne upon the death of which monarch?",
   answers: ["Queen Victoria", "King Edward VI", "King George III", "Queen Elizabeth I"],
   correctAnswer: 0,
   reply: "Queen Victoria died on 22 January 1901 and was succeeded by her eldest son, Edward, Prince of Wales, coronated 9 August 1902 as Edward VII.",
-  correctReply: "Correct!" 
-  incorrectReply: "Incorrect." 
+  // correctReply: "Correct!", 
+  // incorrectReply: "Incorrect.",
   points: 100
 };
 
